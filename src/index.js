e63912889aaa2c73bc6faea75ebf3607b856a50c
Tomáš Trejdl <tom.trejdl@seznam.cz>
2020-03-12 23:38:52 +0100

    Fixed 404 on reload

--- src/index.js
+++ src/index.js
@@ -16,18 +16,19 @@ app.use(express.json());
 app.use(cors());
 app.options('*', cors()); // include before other routes
 
-app.use('/api/v1', apiV1Router);
-
-// Serve static assets for frontend
-app.use(express.static('frontend-dist'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend-dist', 'index.html'));
-});
-
 app.get('/api', (req, res) => {
   res.send(`Use GET /api/v1/status to get the current state.
     Use POST /api/v1/order with {targetState: state} to turn on/off.`);
+});
+
+/* API */
+app.use('/api/v1', apiV1Router);
+
+/* Serve static assets for frontend */
+app.use(express.static('frontend-dist'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend-dist', 'index.html'));
 });
 
 http.listen(process.env.PORT);
